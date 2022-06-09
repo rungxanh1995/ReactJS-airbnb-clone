2c40066e5e46f60a7b5b61e56237ff9fd66b74ca
Joe Pham <rungxanh2901@gmail.com>
2022-06-09 17:33:52 -0400

    Initial upload

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+	return (
+		<nav>
+			<img src='/images/airbnb-logo.png' alt="AirBnb Logo" className="nav--logo"/>
+		</nav>
+	)
+}
+
+export default Navbar
